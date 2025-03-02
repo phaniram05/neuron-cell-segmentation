df40481ab1e1f236434b25e0824e912f1465a6e0
saiphaniram <saiphaniram98@gmail.com>
2025-03-02 15:30:27 -0600

    Updated Version

--- app.py
+++ app.py
@@ -23,12 +23,7 @@ st.markdown(
 
 
 # Load your pre-trained U-net model from a .pkl file with CPU mapping
-from functools import partial
-
-pickle.load = partial(pickle.load, encoding="latin1")
-pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
-
-model = torch.load('entire_model.pkl', map_location=torch.device('cpu'), pickle_module=pickle)
+model = torch.load('entire_model.pkl', map_location=torch.device('cpu'))
 model.eval()
 
 # Define a function to preprocess the image
